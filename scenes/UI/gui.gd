dfb6a39d9abcd97cd2795607580c986e5dc3b981
gongasmouroni <goncalo.pedreno@gmail.com>
2026-01-31 12:34:44 +0000

    Fixed mask change

--- scenes/UI/gui.gd
+++ scenes/UI/gui.gd
@@ -4,7 +4,7 @@ extends Control
 @onready var canvas_modulate: CanvasModulate = $CanvasModulate
 @onready var texture_rect: TextureRect = $TextureRect
 
-var paths = ["res://assets/Masks/Red_Carnival_Mask_PNG_Clip_Art_Image.png", "res://assets/Masks/oni.png"]
+var paths = ["res://assets/Masks/Red_Carnival_Mask_PNG_Clip_Art_Image.png", "res://assets/Masks/oni.png", "res://assets/Masks/Mask-1.png"]
 var index = 0
 
 var isApplied = false
@@ -16,7 +16,7 @@ func _unhandled_input(_event: InputEvent) -> void:
 			canInteract = false
 			animation_player.stop()
 			animation_player.play("change_mask")
-			index = (index + 1) % 2
+			index = (index + 1) % 3
 			await get_tree().create_timer(0.4, true).timeout
 			texture_rect.texture = load(paths[index])
 			await get_tree().create_timer(0.9, true).timeout
